5c2033c84a449ac820190f62b4228dc785001b1f
Ned Palacios <npdoesmc@gmail.com>
2020-05-06 13:45:26 +0800

    0,2

--- jsonrpc.v
+++ jsonrpc.v
@@ -1,13 +1,11 @@
 module jsonrpc
 
-import (
-	net
-	json
-	log
-	http
-)
+import json
+import net.http
+import strings
 
 pub const (
+	JRPC_VERSION = '2.0'
     PARSE_ERROR = -32700
     INVALID_REQUEST = -32600
     METHOD_NOT_FOUND = -32601
@@ -19,70 +17,30 @@ pub const (
     UNKNOWN_ERROR = -32001
 )
 
-const (
-    JRPC_VERSION = '2.0'
-)
+type ProcFunc = fn (ctx mut Context) string
 
 pub struct Context {
 pub mut:
 	res Response
 	req Request
-	raw RawRequest
 }
 
-struct Procedure {
-	name string
-	func fn (Context) string
-}
-
-// struct Procedure<T> {
-// 	name string
-// 	func fn (Context) string
-// 	result T
-// }
-
-struct RawRequest {
+struct Request {
 mut:
-    jsonrpc string
+    jsonrpc string = JRPC_VERSION
     id int
     method string
 	headers map[string]string [skip]
     params string [raw]
 }
 
-pub struct Request {
-pub:
-    jsonrpc string
-    id int
-    method string
-mut:
-    params map[string]string
-}
-
-// pub struct Request<T> {
-// pub:
-//     jsonrpc string
-//     id int
-//     method string
-// mut:
-//     params T
-// }
-
 pub struct Response {
-    jsonrpc string
+    jsonrpc string = JRPC_VERSION
 mut:
     id int
-    error ResponseError [json:error]
+    error ResponseError
     result string
 }
-
-// pub struct Response<T> {
-//     jsonrpc string
-// mut:
-//     id int
-//     error ResponseError [json:error]
-//     result T
-// }
 
 struct ResponseError {
 mut:
@@ -93,17 +51,18 @@ mut:
 
 pub struct Server {
 mut:
-	port int
-	procs []Procedure
+	procs map[string]ProcFunc = map[string]ProcFunc
 }
 
 pub fn (res mut Response) send_error(err_code int) {
-	mut error := ResponseError{ code: err_code, data: '' }
-	error.message = err_message(err_code)
-	res.error = error
+	res.error = ResponseError{ 
+		code: err_code, 
+		data: '', 
+		message: err_message(err_code) 
+	}
 }
 
-fn err_message(err_code int) string {
+pub fn err_message(err_code int) string {
 	msg := match err_code {
 		PARSE_ERROR { 'Invalid JSON' }
 		INVALID_PARAMS { 'Invalid params.' }
@@ -118,149 +77,82 @@ fn err_message(err_code int) string {
 	return msg
 }
 
-fn find_between(s string, start, end string) string {
-	start_pos := s.index(start)
-	if start_pos == -1 {
-		return ''
-	}
-
-	val := s.right(start_pos + start.len)
-	end_pos := val.last_index(end)
-	if end_pos == -1 {
-		return val
-	}
-	return val.left(end_pos)
-}
-
-
-fn (res Response) json() string {
-	mut res_json_arr := []string
-
-	res_json_arr << '"jsonrpc":"${res.jsonrpc}"'
-	
-	if res.id != 0 {
-		res_json_arr << '"id":${res.id}'
-	}
-
+pub fn (res &Response) gen_json() string {
+	mut js := strings.new_builder(5000)
+	js.write('{"jsonrpc":"${res.jsonrpc}"')
+	if res.id != 0 { js.write(',"id":${res.id}') }
 	if res.error.message.len != 0 {
-		res_json_arr << '"error": {"code":${res.error.code},"message":"${res.error.message}","data":"${res.error.data}"}'
+		js.write(',"error":${res.error.gen_json()}')
 	} else {
-		res_json_arr << '"result":"${res.result}"'
+		js.write(',"result":')
+		if res.result[0].is_digit() || res.result[0] in [`{`, `[`] {
+			js.write(res.result)
+		} else {
+			js.write('"${res.result}"')
+		}
 	}
 
-	return '{' + res_json_arr.join(',') + '}'
+	js.write('}')
+	return js.str()
 }
 
-fn (err ResponseError) str() string {
-	return json.encode(err)
+pub fn (err &ResponseError) gen_json() string {
+	return '{"code":${err.code},"message":"${err.message}","data":"${err.data}"}'
 }
 
-fn (res &Response) send(conn net.Socket) {
-	res_json := res.json()
-
-	conn.write('Content-Length: ${res_json.len}\r')
-	conn.write('')
-	conn.write(res_json)
+pub fn (res &Response) gen_resp_text() string {
+	js := res.gen_json()
+	return 'Content-Length: ${js.len}\r\n\n${js}'
 }
 
-fn process_request(raw_req RawRequest) Request {
-	mut req := Request{JRPC_VERSION, raw_req.id, raw_req.method, map[string]string}
-	params_arr := find_between(raw_req.params, '{', '}').split(',')
-
-	for pkv in params_arr {
-		p := pkv.split(':')
-		key := p[0].find_between('"', '"')
-		val := p[1].find_between('"', '"')
-
-		req.params[key] = val
-	}
-
+fn process_request(js_str string) Request {
+	if js_str == '{}' { return Request{} }
+	req := json.decode(Request, js_str) or { return Request{} }
 	return req
 }
 
-fn (server Server) proc_index(name string) int {
-	for i, proc in server.procs {
-		if proc.name == name {
-			return i
-		}
+pub fn (srv Server) exec(incoming string) ?Response {
+	vals := incoming.split_into_lines()
+	content := vals[vals.len-1]
+
+	if incoming.len == 0 {
+		internal_err := INTERNAL_ERROR
+		return error(internal_err.str())
 	}
 
-	return -1
-}
+	if content in ['{}', ''] || vals.len < 2 {
+		invalid_req := INVALID_REQUEST
+		return error(invalid_req.str())
+	}
 
-fn process_raw_request(json_str string, raw_contents string) RawRequest {
-	mut raw_req := RawRequest{}
-	raw_req.headers = http.parse_headers(raw_contents.split_into_lines())
+	mut req := process_request(content, incoming)
+	req.headers = http.parse_headers(incoming.split_into_lines())
+	mut res := Response{ id: req.id }
+	ctx := Context{res, req}
 
-	if json_str == '{}' {
-		return raw_req
+	if req.method in srv.procs.keys() {
+		proc := srv.procs[req.method]
+		res.result = proc(ctx)
 	} else {
-		from_json := json.decode(RawRequest, json_str) or { return raw_req }
-		raw_req.jsonrpc = from_json.jsonrpc
-		raw_req.id = from_json.id
-		raw_req.method = from_json.method
-		raw_req.params = from_json.params
+		method_nf := METHOD_NOT_FOUND
+		return error(method_nf.str())
 	}
 
-	return raw_req
+	return res
 }
 
-pub fn (server mut Server) start_and_listen(port_num int) {
-	server.port = port_num
-
-	listener := net.listen(server.port) or {panic('Failed to listen to port ${server.port}')}
-	mut logg := log.Log{ level: 4, output: 'terminal' }
-
-	logg.info('JSON-RPC Server has started on port ${server.port}')
-	for {
-		mut res := Response{ jsonrpc: JRPC_VERSION }
-		conn := listener.accept() or {
-			logg.set_level(1)
-			logg.error(err_message(SERVER_ERROR_START))
-			res.send_error(SERVER_ERROR_START)
-			return
-		}
-		s := conn.read_line()
-		vals := s.split_into_lines()
-		content := vals[vals.len-1]
-		raw_req := process_raw_request(content, s)
-		req := process_request(raw_req) 
-
-		if s == '' {
-			logg.set_level(2)
-			logg.error(err_message(INTERNAL_ERROR))
-			res.send_error(INTERNAL_ERROR)
-		}
-
-		if content == '{}' || content == '' || vals.len < 2 {
-			logg.set_level(2)
-			logg.error(err_message(INVALID_REQUEST))
-			res.send_error(INVALID_REQUEST)
-		}
-
-		res.id = req.id
-		proc_idx := server.proc_index(req.method)
-		ctx := Context{res: res, req: req, raw: raw_req}
-
-		if proc_idx != -1 {
-			invoke_proc := server.procs[proc_idx].func
-			proc_name := server.procs[proc_idx].name
-			res.result = invoke_proc(ctx)
-			logg.set_level(4)
-			logg.info('[ID: ${req.id}][${req.method}] ${raw_req.params}')
-		}
-
-		res.send(conn)
-		conn.close() or { return }
-	}
-}
-
-// pub fn (server mut Server) register_procedure<T,U>(method_name string, proc_func fn (Context) T, result_typ U)
-pub fn (server mut Server) register_procedure(method_name string, proc_func fn (Context) string) {
-	proc := Procedure{ name: method_name, func: proc_func }
-	server.procs << proc
+pub fn (srv mut Server) register(name string, func ProcFunc) {
+	srv.procs[name] = func
 }
 
 pub fn new() Server {
-	return Server{ port: 8046, procs: []Procedure }
+	return Server{ procs: map[string]ProcFunc }
+}
+
+pub fn as_array(p string) []string {
+	return p.find_between('[',']').split(',')
+}
+
+pub fn as_string(p string) string {
+	return p.find_between('"', '"')
 }
